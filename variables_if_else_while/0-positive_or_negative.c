302a7ab78db1b918f639c2e2eb4de862563001ce
CodexSC <carlos.silva@holbertonstudents.com>
2025-10-24 15:18:35 +0200

    Commit All

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates a random integer and prints whether it is
+ *        positive, negative, or zero
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	/* Seed the random number generator */
+	srand(time(0));
+
+	/* Generate a random number between -RAND_MAX/2 and RAND_MAX/2 */
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+
+	return (0);
+}
