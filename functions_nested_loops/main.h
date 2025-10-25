618a07fdb3ddd428415afbd75b48a50e2154cdf0
CodexSC <carlos.silva@holbertonstudents.com>
2025-10-25 14:01:01 +0200

    main.h corrected push

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,23 +1,6 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - Entry point
- *
- * Description: Prints _putchar followed by a new line
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+int _putchar(char c);
 
-	return (0);
-}
+#endif /* MAIN_H */
