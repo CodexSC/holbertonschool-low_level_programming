431ea9156d46e66b1ccc21db41d27073d68b652e
CodexSC <carlos.silva@holbertonstudents.com>
2025-10-25 14:15:58 +0200

    Error with main

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,15 +1,13 @@
+#include <unistd.h>
 #include "main.h"
 
 /**
- * print_alphabet - prints the alphabet in lowercase
- * followed by a new line
+ * _putchar - writes character c to stdout
+ * @c: character to print
+ *
+ * Return: 1 on success, -1 on error
  */
-void print_alphabet(void)
+int _putchar(char c)
 {
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-
-	_putchar('\n');
+    return (write(1, &c, 1));
 }
