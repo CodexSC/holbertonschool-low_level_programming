defca9ac10e5bfa176081e1cb822644df337e214
CodexSC <carlos.silva@holbertonstudents.com>
2025-10-25 15:11:23 +0200

    Error corrected

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -10,11 +10,11 @@ int print_last_digit(int n)
 {
 	int last;
 
-	if (n < 0)
-		n = -n;
-
 	last = n % 10;
-	_putchar(last + '0'); /* print last digit */
+	if (last < 0)
+		last = -last;
+
+	_putchar(last + '0');
 
 	return (last);
 }
