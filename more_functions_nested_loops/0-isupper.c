ab416f6fabe4e1e543204b62a117813d56323112
CodexSC <carlos.alexandur@gmail.com>
2025-10-28 14:24:04 +0100

    Commit Code Nested 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints _isupper followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+if (c == 67)
+putchar('1')
+else
+putchar('0')
+
+return(0)
+return(1)
+}
