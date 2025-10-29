2a7918795606a861832768806a7f692b8be89522
CodexSC <carlos.alexandur@gmail.com>
2025-10-29 22:26:48 +0100

    4-print_most _numbers all checkd

--- more_functions_nested_loops/4-main.c
+++ more_functions_nested_loops/4-main.c
@@ -10,4 +10,3 @@ int main(void)
     print_most_numbers();
     return (0);
 }
-julien@ubuntu:~/$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 4-main.c 4-print_most_numbers.c -o 4-print_most_numbers
--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_numbers - prints the numbers from 0 to 9
+ * print_most_numbers - prints the numbers from 0 to 9, except 2 and 4
  *
  * Return: void
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
-	int i;
-	
-	for (i = 0; i < 10; i++)
+	int p;
+
+	for (p = 0; p <= 9; p++)
 	{
-		_putchar('0' + i);
+		if (p != 2 && p != 4)
+		{
+			_putchar(p + '0');
+		}
 	}
 	_putchar('\n');
 }
