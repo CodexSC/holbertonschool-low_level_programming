37936fe7d8e1440b76e9d936fb60a6cb0d3c2cd8
CodexSC <carlos.alexandur@gmail.com>
2025-11-01 16:58:24 +0100

    New synthax commit

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,32 +1,30 @@
-#include "main.h"
-
-/**
- * _atoi - convert a string to an integer
- * @s: the string to convert
- *
- * Return: the integer value of the string
- */
 int _atoi(char *s)
 {
-    int i = 0, sign = 1;
-    long result = 0; /* use long to prevent overflow during conversion */
+	int i = 0;
+	int sign = 1;
+	int result = 0;
+	int started = 0;
 
-    /* skip non-digit characters and detect sign */
-    while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
-    {
-        if (s[i] == '-')
-            sign *= -1;
-        i++;
-    }
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
 
-    /* convert digits to number */
-    while (s[i] >= '0' && s[i] <= '9')
-    {
-        result = result * 10 + (s[i] - '0');
-        i++;
-    }
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			started = 1;
+			result = result * 10 - (s[i] - '0');
+		}
+		else if (started)
+		{
+			break;
+		}
 
-    result *= sign;
+		i++;
+	}
 
-    return (int)result;
+	if (sign > 0)
+		result = -result;
+
+	return result;
 }
