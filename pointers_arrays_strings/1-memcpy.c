7e410e6792a9927268ac1a2a48d2171de72858ee
CodexSC <carlos.alexandur@gmail.com>
2025-11-03 15:48:53 +0100

    Commit 1-memcpy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: starting number
+ */
+
+ char *_memcpy(char *dest, char *src, unsigned int n);
+
+ unsigned int n;
+ {
+    for (n = 0; n != '\0'; n++);
+    {
+        dest[n] = src[n];
+    }
+
+ return (dest);
+}
