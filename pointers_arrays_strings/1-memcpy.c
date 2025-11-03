70cd601b609a9c3dc9ecf6de8a0c843744f7d51a
CodexSC <carlos.alexandur@gmail.com>
2025-11-03 16:04:41 +0100

    Betty corrected

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * print_to_98 - prints all natural numbers from n to 98
- * @n: starting number
+ * _memcpy - function that copies memory area
+ * @dest: destination memory area
+ * @src: source memory area
+ * @n: number of bytes to copy
+ *
  * Return: pointer to dest
- * Description: copies n bytes from memory area src to memory area dest
  */
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 
 {
