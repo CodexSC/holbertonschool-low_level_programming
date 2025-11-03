75fb74cce1f331d552390f964bdb2ec4cd368e57
CodexSC <carlos.alexandur@gmail.com>
2025-11-03 14:53:58 +0100

    Commit

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -9,15 +9,15 @@
  *
  * Return: pointer to the memory area s
  */
+char *_memset(char *s, char b, unsigned int n)
 
- char *_memset(char *s, char b, unsigned int n)
-
- int n;
- {
+{
     unsigned int i;
+
     for (i = 0; i < n; i++)
     {
         s[i] = b;
     }
-    return s;
- }
+
+    return (s);
+}
