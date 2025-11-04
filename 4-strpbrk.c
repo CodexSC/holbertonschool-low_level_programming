15a6da10bf2116ebf58abf609af4eeb41512ac9f
CodexSC <carlos.alexandur@gmail.com>
2025-11-04 18:13:43 +0100

    Commit all files corrected

--- 4-strpbrk.c
+++ 4-strpbrk.c
@@ -1,15 +1,30 @@
 #include "main.h"
 #include <stdio.h>
 
-char *_strchr(char *s, char c)
+/**
+ * _strpbrk - Searches a string for any of a set of bytes
+ * @s: Pointer to the string to search
+ * @accept: Pointer to the string of characters to match
+ *
+ * Return: Pointer to the first character in s that
+ * matches any character in accept,
+ *         or NULL if no match is found
+ */
+
+char *_strpbrk(char *s, char *accept)
 {
-    while (*s)
-    {
-        if (*s == c);
-            return (s);
-        s++;
-    }
-    if (*s == c)
-        return (s);
-    return (NULL);
+	int i;
+
+	while (*s != '\0')
+	{
+		i = 0;
+		while (accept[i] != '\0')
+		{
+			if (*s == accept[i])
+				return (s);
+			i++;
+		}
+		s++;
+	}
+	return (NULL);
 }
