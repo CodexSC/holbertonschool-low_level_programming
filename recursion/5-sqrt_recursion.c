53293124c814f4088e55e5c57b3f6c38d2cd5973
CodexSC <carlos.alexandur@gmail.com>
2025-11-05 17:17:30 +0100

    linter checked

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -4,12 +4,15 @@
 int _sqrt_helper(int n, int i);
 
 /**
- * _sqrt_recursion - returns the natural square root of a number using recursion
+ * _sqrt_recursion - returns the natural square root
+ * of a number using recursion
  * @n: the number to find the square root of
  *
- * Return: natural square root of n, or -1 if n does not have a natural square root
+ * Return: natural square root of n, or -1
+ * if n does not have a natural square root
  *
- * Description: Uses a helper function to recursively find the natural square root
+ * Description: Uses a helper function
+ * to recursively find the natural square root
  */
 int _sqrt_recursion(int n)
 {
