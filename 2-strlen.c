a7ece3a0ed8a2972c21fac95c66bacda95269b46
CodexSC <carlos.alexandur@gmail.com>
2025-11-06 13:49:35 +0100

    Push strlen

--- 2-strlen.c
+++ 2-strlen.c
@@ -1,21 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _strchr - Locates a character in a string
- * @s: Pointer to the string to search
- * @c: Character to locate
+ * _strlen - Returns the length of a string
+ * @s: Pointer to the string
  *
- * Return: Pointer to the first occurrence of the character,
- *         or NULL if not found
+ * Return: The length of the string
  */
-char *_strchr(char *s, char c)
+int _strlen(char *s)
 {
-	while (*s)
-	{
-		if (*s == c)
-			return (s);
-		s++;
-	}
-	return (*s == c ? s : NULL);
+	int length = 0;
+
+	while (s[length] != '\0')
+		length++;
+
+	return (length);
 }
