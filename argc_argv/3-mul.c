42cbb20dd6dff9570d1f996c75c958fee44d83de
CodexSC <carlos.alexandur@gmail.com>
2025-11-07 16:46:08 +0100

    Push files

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,12 +1,31 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _atoi - converts string to integer
+ * @s: string
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+    int i = 0, sign = 1, num = 0;
+
+    if (s[0] == '-')
+    {
+        sign = -1;
+        i++;
+    }
+
+    for (; s[i]; i++)
+        num = num * 10 + (s[i] - '0');
+
+    return sign * num;
+}
 
 /**
  * print_number - prints an integer using _putchar
  * @n: number to print
  */
-
 void print_number(int n)
 {
     if (n < 0)
@@ -14,15 +33,13 @@ void print_number(int n)
         _putchar('-');
         n = -n;
     }
-
     if (n / 10)
         print_number(n / 10);
-
     _putchar((n % 10) + '0');
 }
 
 /**
- * main - multiplies two numbers passed as arguments
+ * main - multiplies two numbers from arguments
  * @argc: argument count
  * @argv: argument vector
  * Return: 0 on success, 1 on error
@@ -30,21 +47,18 @@ void print_number(int n)
 int main(int argc, char **argv)
 {
     int num1, num2, result;
+    int i;
 
     if (argc != 3)
     {
         char *error = "Error\n";
-        int i = 0;
-        while (error[i])
-        {
+        for (i = 0; error[i]; i++)
             _putchar(error[i]);
-            i++;
-        }
         return 1;
     }
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[2]);
+    num1 = _atoi(argv[1]);
+    num2 = _atoi(argv[2]);
     result = num1 * num2;
 
     print_number(result);
