7d42d9f42ff95651bf116b2487e5e7641e3cd9aa
CodexSC <carlos.alexandur@gmail.com>
2025-11-07 16:35:20 +0100

    Commit compiled files

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,6 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+
 int main(int argc, char **argv)
 {
-    (void)argv; /* suppress unused parameter warning */
+    (void)argv;  /* suppress unused parameter warning */
 
     _putchar((argc - 1) + '0');
     _putchar('\n');
