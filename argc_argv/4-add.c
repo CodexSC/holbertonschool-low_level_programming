aa8c0ea34305c4d98ccfb921b90323c112db06e9
CodexSC <carlos.alexandur@gmail.com>
2025-11-07 17:00:42 +0100

    Push add .c file

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,83 +1,67 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 /**
- * is_digit_string - checks if a string contains only digits
- * @s: string to check
- * Return: 1 if all digits, 0 otherwise
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int is_digit_string(char *s)
+int _putchar(char c)
 {
-    int i = 0;
-
-    if (!s || !s[0])
-        return 0;
-
-    while (s[i])
-    {
-        if (s[i] < '0' || s[i] > '9')
-            return 0;
-        i++;
-    }
-    return 1;
+	return (putchar(c));
 }
 
 /**
- * print_number - prints an integer using _putchar
- * @n: number to print
+ * is_valid_number - checks if string contains only digits
+ * @str: string to check
+ *
+ * Return: 1 if valid, 0 otherwise
  */
-void print_number(int n)
+int is_valid_number(char *str)
 {
-    if (n / 10)
-        print_number(n / 10);
-    _putchar((n % 10) + '0');
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 /**
- * main - adds positive numbers from command-line arguments
- * @argc: number of arguments
+ * main - adds positive numbers passed as arguments
+ * @argc: argument count
  * @argv: argument vector
+ *
  * Return: 0 on success, 1 on error
  */
 int main(int argc, char **argv)
 {
-    int i, sum = 0;
-    int num;
+	int sum;
+	int i;
+	int num;
 
-    if (argc == 1)
-    {
-        _putchar('0');
-        _putchar('\n');
-        return 0;
-    }
+	sum = 0;
+	i = 1;
 
-    for (i = 1; i < argc; i++)
-    {
-        if (!is_digit_string(argv[i]))
-        {
-            char *error = "Error\n";
-            int j = 0;
-            while (error[j])
-            {
-                _putchar(error[j]);
-                j++;
-            }
-            return 1;
-        }
+	while (i < argc)
+	{
+		if (!is_valid_number(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		num = atoi(argv[i]);
+		sum += num;
+		i++;
+	}
 
-        num = 0;
-        int k = 0;
-        while (argv[i][k])
-        {
-            num = num * 10 + (argv[i][k] - '0');
-            k++;
-        }
-
-        sum += num;
-    }
-
-    print_number(sum);
-    _putchar('\n');
-
-    return 0;
+	printf("%d\n", sum);
+	return (0);
 }
