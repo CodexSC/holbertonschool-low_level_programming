53e96fe1b3543d606aab4833a07fc581fa6a8034
CodexSC <carlos.alexandur@gmail.com>
2025-11-10 14:43:09 +0100

    Push Compiled file

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -6,6 +6,7 @@ char *create_array(unsigned int size, char c)
 
 {
 char *array;
+unsigned int i;
 
     if(size == 0)
     return(NULL);
@@ -14,7 +15,7 @@ char *array;
     if (array == NULL)
         return (NULL);
 
-    for (unsigned int i = 0; i < size; i++)
+    for (i = 0; i < size; i++)
         array[i] = c;
 
 return(array);
