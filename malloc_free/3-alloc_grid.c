15089c44a3e83dc8da74e636c9b56c64605985e5
CodexSC <carlos.alexandur@gmail.com>
2025-11-12 16:39:43 +0100

    Betty documentation inserted

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -3,6 +3,18 @@
 #include <string.h>
 #include "main.h"
 
+/**
+ * alloc_grid - Allocates memory for a 2D array of integers
+ * @width: Width of the grid (number of columns)
+ * @height: Height of the grid (number of rows)
+ *
+ * Description: Allocates memory for a 2D array where each element
+ * is initialized to 0. Returns a pointer to the newly allocated grid.
+ * Caller is responsible for freeing the memory.
+ *
+ * Return: Pointer to 2D array (int **), or NULL on failure
+ */
+
 int **alloc_grid(int width, int height)
 
 {
