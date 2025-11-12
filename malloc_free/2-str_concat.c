b80c522c4159c68a04c2701f6cf6727c33b7bb76
CodexSC <carlos.alexandur@gmail.com>
2025-11-12 16:06:27 +0100

    Code commit and push, betty checked

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -5,27 +5,43 @@
 
 char *str_concat(char *s1, char *s2)
 {
-    char *result;
-    size_t len1 = 0, len2 = 0;
+	char *result;
+	size_t len1 = 0, len2 = 0;
+	size_t i = 0;
 
-    if (s1 != NULL)
-        len1 = strlen(s1);
-    if (s2 != NULL)
-        len2 = strlen(s2);
-
-    result = malloc(len1 + len2 + 1);
-    if (result == NULL)
-        return NULL;
-
-    if (s1 != NULL)
-        strcpy(result, s1);
-    else
-        result[0] = '\0';
-
-    if (s2 != NULL)
-        strcpy(result + len1, s2);
-    else
-        result[len1] = '\0';
-
-    return result;
+	if (s1 != NULL)
+	{
+		while (s1[i] != '\0')
+			i++;
+		len1 = i;
+	}
+	i = 0;
+	if (s2 != NULL)
+	{
+		while (s2[i] != '\0')
+			i++;
+		len2 = i;
+	}
+	result = malloc(len1 + len2 + 1);
+	if (result == NULL)
+		return (NULL);
+	i = 0;
+	if (s1 != NULL)
+	{
+		while (s1[i] != '\0')
+		{
+			result[i] = s1[i];
+			i++;
+		}
+	}
+	if (s2 != NULL)
+	{
+		while (s2[i - len1] != '\0')
+		{
+			result[i] = s2[i - len1];
+			i++;
+		}
+	}
+	result[i] = '\0';
+	return (result);
 }
