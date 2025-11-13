c83cafa0972c28702841931f99c4d7c1d2c14a21
CodexSC <carlos.alexandur@gmail.com>
2025-11-13 16:18:24 +0100

    Files pushed betty linter checked

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,26 +1,25 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
 /**
  * malloc_checked - Allocates memory using malloc
  * @b: Number of bytes to allocate
  *
- * Description: Allocates memory of @b bytes using malloc.
- * If malloc fails, the program exits with status code 98.
+ * Return: A pointer to the allocated memory
  *
- * Return: Pointer to the allocated memory
+ * Description: This function wraps malloc and performs error checking.
+ * If malloc fails to allocate memory, the program exits with status code 98.
+ * This ensures that the calling code can safely assume memory allocation
+ * succeeded without needing to check for NULL.
  */
 
 void *malloc_checked(unsigned int b)
 
 {
-void *ptr;
-
-ptr = malloc(b);
+int *ptr = malloc(sizeof(b));
 
 if (ptr == NULL)
 	exit(98);
+
 return (ptr);
 }
