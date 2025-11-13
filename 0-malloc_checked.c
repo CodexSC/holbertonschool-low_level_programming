2cbf346893af5ee3a43fa8f995748b8798dfbdeb
CodexSC <carlos.alexandur@gmail.com>
2025-11-13 10:41:54 +0100

    Push files

---
+++ 0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+
+void *malloc_checked(unsigned int b)
+{
+void *ptr;
+
+ptr = malloc(b);
+
+ptr = malloc(b);
+if (ptr == NULL)
+    exit(98);
+return(ptr);
+}
