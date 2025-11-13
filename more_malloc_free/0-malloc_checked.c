79a2b2bd4bdf42ed6d8735e9ef73925cd868b345
CodexSC <carlos.alexandur@gmail.com>
2025-11-13 11:18:31 +0100

    Betty documentation style

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -3,6 +3,15 @@
 #include <stdlib.h>
 #include <limits.h>
 
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: Number of bytes to allocate
+ *
+ * Description: Allocates memory of @b bytes using malloc.
+ * If malloc fails, the program exits with status code 98.
+ *
+ * Return: Pointer to the allocated memory
+ */
 
 void *malloc_checked(unsigned int b)
 
@@ -12,6 +21,6 @@ void *ptr;
 ptr = malloc(b);
 
 if (ptr == NULL)
-    exit(98);
-return(ptr);
+	exit(98);
+return (ptr);
 }
