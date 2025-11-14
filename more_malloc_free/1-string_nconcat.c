e5c0a633eb1107604dcabf9c05b2b1212242c44d
CodexSC <carlos.alexandur@gmail.com>
2025-11-14 15:01:35 +0100

    Synthax corrected, pushed

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <string.h>
 #include "main.h"
 
 /**
@@ -14,33 +13,59 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *result;
-	unsigned int len1, len2, total_len;
+	unsigned int len1, len2, i;
 
-	/* Treat NULL as empty string */
-	len1 = s1 ? strlen(s1) : 0;
-	len2 = s2 ? strlen(s2) : 0;
+	/* Calculate length of s1 manually (treat NULL as empty) */
+	len1 = 0;
+	if (s1)
+	{
+		while (s1[len1])
+			len1++;
+	}
+
+	/* Calculate length of s2 manually (treat NULL as empty) */
+	len2 = 0;
+	if (s2)
+	{
+		while (s2[len2])
+			len2++;
+	}
 
 	/* If n is greater than or equal to len2, use entire s2 */
 	if (n >= len2)
 		n = len2;
 
-	/* Calculate total length needed (s1 + n bytes from s2 + null terminator) */
-	total_len = len1 + n + 1;
-
 	/* Allocate memory for the concatenated string */
-	result = malloc(total_len);
+	result = malloc(len1 + n + 1);
 	if (result == NULL)
 		return (NULL);
 
 	/* Copy s1 into result */
+	i = 0;
 	if (s1)
-		strcpy(result, s1);
-	else
-		result[0] = '\0';
+	{
+		while (s1[i])
+		{
+			result[i] = s1[i];
+			i++;
+		}
+	}
 
-	/* Concatenate first n bytes of s2 */
+	/* Copy first n bytes of s2 */
 	if (s2)
-		strncat(result, s2, n);
+	{
+		unsigned int j = 0;
+
+		while (j < n)
+		{
+			result[i] = s2[j];
+			i++;
+			j++;
+		}
+	}
+
+	/* Null terminate */
+	result[i] = '\0';
 
 	return (result);
 }
