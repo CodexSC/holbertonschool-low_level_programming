58da2bf015457074c276e6c4d3c9e5c0cd477a1a
CodexSC <carlos.alexandur@gmail.com>
2025-11-17 17:29:14 +0100

    Betty documenting added

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,13 @@
 #include "function_pointers.h"
 
+   /**
+    * print_name - Prints a name using a function pointer.
+    * @name: The name to print.
+    * @f: A function pointer that takes a string as an argument and returns nothing.
+    *
+    * Return: Nothing.
+    */
+
 void print_name(char *name, void (*f)(char *))
 {
 if (name == NULL || f == NULL)
