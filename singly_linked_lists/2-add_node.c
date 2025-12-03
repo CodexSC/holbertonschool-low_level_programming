4d0a236197e3c4570ac0a16ab1aea5cb3e7bfbce
CodexSC <carlos.alexandur@gmail.com>
2025-12-03 19:00:56 +0100

    Style changes. Testing...

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -16,17 +16,14 @@ list_t *add_node(list_t **head, const char *str)
 	new_node = malloc(sizeof(list_t));
 	if (!new_node)
 		return (NULL);
-
 	new_node->str = strdup(str);
 	if (!new_node->str)
 	{
 		free(new_node);
 		return (NULL);
 	}
-
 	new_node->len = strlen(new_node->str);
 	new_node->next = *head;
 	*head = new_node;
-
 	return (new_node);
 }
