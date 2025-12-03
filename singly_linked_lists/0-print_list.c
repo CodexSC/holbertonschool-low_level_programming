9f8b530808e2dfb6481fd6e4fbf6df24ea95a64e
CodexSC <carlos.alexandur@gmail.com>
2025-12-03 17:11:20 +0100

    Added node files and pushed

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,41 +1,20 @@
 #include "lists.h"
 #include <stdio.h>
 
-/**
- * print_list - Prints all elements of a singly linked list
- * @h: Pointer to the head of the list
- *
- * Return: The number of nodes in the list
- *
- * Description: Prints each node in format [length] string
- * If str is NULL, prints [0] (nil)
- */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+    size_t count = 0;
 
-	/* Traverse the entire linked list */
-	while (h != NULL)
-	{
-		/* Check if str pointer is NULL */
-		if (h->str == NULL)
-		{
-			/* Print [0] (nil) for NULL strings */
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-			/* Print [length] string for valid strings */
-			printf("[%u] %s\n", h->len, h->str);
-		}
+    while (h != NULL)
+    {
+        if (h->str == NULL)
+            printf("[0] (nil)\n");
+        else
+            printf("[%u] %s\n", h->len, h->str);
 
-		/* Increment node counter */
-		count++;
+        count++;
+        h = h->next;
+    }
 
-		/* Move to the next node */
-		h = h->next;
-	}
-
-	/* Return the total number of nodes */
-	return (count);
+    return count;
 }
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -3,6 +3,12 @@
 
 #include <stdlib.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * @len: length of the string
+ * @next: points to the next node
+*/
 typedef struct list_s
 {
     char *str;
@@ -11,5 +17,7 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 
 #endif
