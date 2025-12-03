fe35ebfea2ca37d7c1fbafc429250dcf28491b43
CodexSC <carlos.alexandur@gmail.com>
2025-12-03 16:53:16 +0100

    Commited files Betty-documention checked.

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,28 +2,40 @@
 #include <stdio.h>
 
 /**
- * print_list - prints all the elements of a linked list_t list
- * @h: pointer to the head of the list
+ * print_list - Prints all elements of a singly linked list
+ * @h: Pointer to the head of the list
  *
- * Return: the number of nodes
+ * Return: The number of nodes in the list
+ *
+ * Description: Prints each node in format [length] string
+ * If str is NULL, prints [0] (nil)
  */
-
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h != NULL)
-        {
-            if (h->str == NULL)
-            {
-                printf("[%u] (nil)\n", h->len);
-            }
-            else
-            {
-            printf("[%u] %s\n", h->len, h->str);
-            }
-        count++;
-        h = h->next;
-        }
-    return (count);
-    }
+	/* Traverse the entire linked list */
+	while (h != NULL)
+	{
+		/* Check if str pointer is NULL */
+		if (h->str == NULL)
+		{
+			/* Print [0] (nil) for NULL strings */
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			/* Print [length] string for valid strings */
+			printf("[%u] %s\n", h->len, h->str);
+		}
+
+		/* Increment node counter */
+		count++;
+
+		/* Move to the next node */
+		h = h->next;
+	}
+
+	/* Return the total number of nodes */
+	return (count);
+}
