e7837897f25ac36d141dd325f18bfd89d02a3af8
CodexSC <carlos.alexandur@gmail.com>
2025-12-03 17:19:13 +0100

    Edited 0-print_list to full proof check

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,20 +1,26 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * print_list - prints all elements of a list_t list
+ * @h: pointer to the list
+ *
+ * Return: the number of nodes
+ */
 size_t print_list(const list_t *h)
 {
     size_t count = 0;
 
-    while (h != NULL)
+    while (h)
     {
         if (h->str == NULL)
             printf("[0] (nil)\n");
         else
             printf("[%u] %s\n", h->len, h->str);
 
-        count++;
         h = h->next;
+        count++;
     }
 
-    return count;
+    return (count);
 }
