7dae925cff72e3055277e61a1c641af11e2a7bac
CodexSC <carlos.alexandur@gmail.com>
2025-12-03 19:47:57 +0100

    Removed strlen and recompiled

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * add_node - adds a new node at the beginning of a list_t list
@@ -12,20 +11,33 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	unsigned int i;
 
-	if (!str)
-		return (NULL);
 	new_node = malloc(sizeof(list_t));
 	if (!new_node)
 		return (NULL);
-	new_node->str = strdup(str);
+
+	/* calculate string length */
+	for (i = 0; str[i]; i++)
+		;
+
+	/* allocate memory for string */
+	new_node->str = malloc(i + 1);
 	if (!new_node->str)
 	{
 		free(new_node);
 		return (NULL);
 	}
-	new_node->len = strlen(new_node->str);
+
+	/* copy string and set length */
+	for (i = 0; str[i]; i++)
+		new_node->str[i] = str[i];
+	new_node->str[i] = '\0';
+	new_node->len = i;
+
+	/* insert at beginning */
 	new_node->next = *head;
 	*head = new_node;
+
 	return (new_node);
 }
