b156cf282ffe800740170dbe583910cf871a96ab
CodexSC <carlos.alexandur@gmail.com>
2025-12-05 18:22:34 +0100

    Built create_file and compiled after style check.

---
+++ file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * create_file - creates a file with text content
+ * @filename: name of the file to create
+ * @text_content: content to write to the file (NULL terminated string)
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_descriptor;
+	ssize_t bytes_written;
+	size_t content_len;
+
+	if (filename == NULL)
+		return (-1);
+
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file_descriptor == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(file_descriptor);
+		return (1);
+	}
+
+	content_len = strlen(text_content);
+	bytes_written = write(file_descriptor, text_content, content_len);
+
+	if (bytes_written == -1 || (size_t)bytes_written != content_len)
+	{
+		close(file_descriptor);
+		return (-1);
+	}
+
+	close(file_descriptor);
+	return (1);
+}
